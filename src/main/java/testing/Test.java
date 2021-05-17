c52c77fdbb3c1b9dc313ac091cc7b7ccda8f5772
Zaid <zaid.kpj@gmail.com>
2021-05-17 12:38:08 +0530

    test

---
+++ src/main/java/testing/Test.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testing;
+
+/**
+ *
+ * @author zaid
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
