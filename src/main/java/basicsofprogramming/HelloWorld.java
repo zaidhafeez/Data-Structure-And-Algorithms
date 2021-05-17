722d9d452e8d3fcc3bf3126c6722245e8c243a8a
Zaid <zaid.kpj@gmail.com>
2021-05-17 12:27:53 +0530

    rename folder

---
+++ src/main/java/basicsofprogramming/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicsofprogramming;
+
+/**
+ *
+ * @author zaid
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("HelloWorld1");
+        System.out.println("hello");
+    }
+    
+}
