c77456df53ac02185a8adf3e67d799359a91744c
Zaid <zaid.kpj@gmail.com>
2021-05-17 12:45:07 +0530

    code changed

--- src/main/java/basicsofprogramming/HelloWorld.java
+++ src/main/java/basicsofprogramming/HelloWorld.java
@@ -12,7 +12,7 @@ package basicsofprogramming;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("HelloWorld1");
-        System.out.println("hello");
+        
     }
     
 }
