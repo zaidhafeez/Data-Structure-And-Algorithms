4de922dfa4a99a7a0340889166563cbb9daaea72
Zaid <zaid.kpj@gmail.com>
2021-05-19 10:02:59 +0530

    My first program

--- src/main/java/basicsofprogramming/HelloWorld.java
+++ src/main/java/basicsofprogramming/HelloWorld.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package basicsofprogramming;
+import java.util.*;
 
 /**
  *
@@ -11,7 +12,32 @@ package basicsofprogramming;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("HelloWorld1");
+        System.out.println("Enter X and Y");
+        Scanner sc = new Scanner(System.in);
+        
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        
+        //Swapping two numbers
+//        
+//        int temp = x;
+//        x = y;
+//        y = temp;
+//        
+//        System.out.println("Swapping x and y are:" + x + " and " + y);
+        
+        //Swapping two numbers without creating any new variable;
+        
+        x = x + y;
+        y = x - y;
+        x = x - y;
+        
+        System.out.println("Swapping x and y are:" + x + " and " + y);
+        
+        
+        
+        
+    
         
     }
     
