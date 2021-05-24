071c7b9d794eb559b79c69a2319c239a0149b929
Zaid <zaid.kpj@gmail.com>
2021-05-24 14:32:36 +0530

    obtain array

--- src/main/java/array/CreateArray.java
+++ src/main/java/array/CreateArray.java
@@ -5,10 +5,43 @@
  */
 package array;
 
+import java.util.Scanner;
+
 /**
  *
  * @author zaid
  */
+class Obtain{
+    
+    int a[] = new int[10];
+    int b[] = new int[10];
+    
+    Obtain(){
+        
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < 10; i++){
+            a[i] = sc.nextInt();
+        }
+    }
+    public int[] getArray(){
+        for(int i = 0; i < 10; i++){
+          b[i] = a[i];
+        }
+        return b;
+    }
+}
 public class CreateArray {
+    public static void main(String[] args) {
+        
+        System.out.println("Enter number in array");
+        
+        Obtain obj = new Obtain();
+        // getting element from the array
+        int a[] = obj.getArray(); 
+        // For Printing array
+        for(int i = 0; i < 10; i++){
+            System.out.print(a[i] + " ");
+        }
+    }
     
 }
