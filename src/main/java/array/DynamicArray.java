ddfb7d7689ec554548d44a306a3255eab1eac127
Zaid <zaid.kpj@gmail.com>
2021-05-26 20:10:33 +0530

    problem solved improvement needed

--- src/main/java/array/DynamicArray.java
+++ src/main/java/array/DynamicArray.java
@@ -5,14 +5,109 @@
  */
 package array;
 
+import java.util.Scanner;
+
 /**
  *
  * @author zaid
  */
+class Array{
+    
+    private int arr[];
+    private int count;
+    
+    public Array(int length){
+        
+        arr = new int[length];
+    
+    }
+    
+    public void printArray(){
+    
+        for(int i = 0; i < count; i++){
+            
+            System.out.print(arr[i] + " ");
+            
+        }
+    
+    }
+    
+    public void insert(int element){
+        
+        if(arr.length == count){
+            
+            int newArr[] = new int[2 * count];
+            
+            for(int i = 0; i < count; i++){
+                
+                newArr[i] = arr[i];
+                
+            }
+            // Assigning new array to original array
+            // created above
+            
+            arr = newArr;
+           
+            
+        }
+        
+        arr[count++] = element;
+        
+    }
+    
+    public boolean checkElement(int checkNumber){
+        
+        boolean found = false;
+        
+        for(int i = 0 ; i < count; i++){
+            
+            if(arr[i] == checkNumber){
+                
+                found = true;
+                break;
+                
+            }
+            else{
+                
+                found = false;
+                
+            }
+            
+        }
+        
+        return found;
+        
+    }
+    
+}
 public class DynamicArray {
     
     public static void main(String[] args) {
-        System.out.println("Hello");
+        
+        System.out.println("Enter the size of array");
+        
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int size = sc.nextInt();
+        
+        
+        
+        Array obj = new Array(size);
+        System.out.println("Enter Check Number");
+        int checkNumber = sc.nextInt();
+        
+        obj.insert(10);
+        obj.insert(20);
+        obj.insert(30);
+        obj.insert(40);
+        obj.insert(50);
+        
+        obj.printArray();
+        System.out.println("");
+        System.out.println(obj.checkElement(checkNumber));
+        
+        
     }
     
 }
